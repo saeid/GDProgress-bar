0e4becf312846f3c2a77ad706d0cb3744a223465
Saeid <saeid@Saeids-MacBook-Air.local>
2016-04-21 21:59:39 +0430

    documenting

--- LoadingBar/ViewController.swift
+++ LoadingBar/ViewController.swift
@@ -9,24 +9,83 @@
 import UIKit
 
 class ViewController: UIViewController, NSURLSessionDownloadDelegate {
-    
+    //Create instance of Progress
     @IBOutlet weak var pView: Progress!
     
+    var isWaiting = false
     var isDownloading = false
     var downloadTask: NSURLSessionDownloadTask?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        pView.setupView()
-        pView.startProgress()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-    //MARK: - Actions
+    func setupView(){
+        //Show labels for download progress
+        pView.showLabels = false
+        
+        //Show route path for a download progress
+        pView.showRoutePath = false
+        
+        //waiting progress animation duration
+        pView.animationTime = 3
+        
+        //Delay between animation for waiting bar
+        pView.animationDelay = 1
+        
+        //path line width for progress/waiting bar
+        pView.lineWidth = 10.0
+        
+        //this will rotate the progress bar if enabled. it works for Waiting bar to create
+        //smooth animating bar
+        pView.shouldRotate = true
+        
+        //active gradiant color for bar
+        pView.shouldGradiant = true
+        
+        //route line color
+        pView.routeColor = UIColor.grayColor()
+        
+        //main progress bar color. this only used for gradiant is not enabled
+        pView.progressColor = UIColor.blueColor()
+        
+        //first gradiant color, this only works if gradiant is enabled
+        pView.grad1Color = UIColor.whiteColor()
+        
+        //second gradiant color, this only works if gradiant is enabled
+        pView.grad2Color = UIColor.blackColor()
+        
+        //font for % label
+        pView.percentFont = UIFont.systemFontOfSize(20)
+        
+        //font for details label
+        pView.detailsFont = UIFont.systemFontOfSize(15)
+        
+        //call this at end of view modifications
+        pView.setupView()
+        
+    }
+    
+    //MARK: - Create Waiting bar
+    @IBAction func startWaiting(){
+        if !isWaiting{
+            //starts waiting bar
+            
+            isWaiting = !isWaiting
+            pView.startProgress()
+        }else{
+            //stop waiting bar
+            
+            isWaiting = !isWaiting
+            pView.endProgress()
+        }
+    }
+    
+    //MARK: - Create Download Actions
     @IBAction func startProgress(sender: UIButton){
         if isDownloading{
             self.isDownloading = !isDownloading
